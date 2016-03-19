d47361d2d295fa7e51c12f9d592750059e35f381
Akihiro Yoza <ic121241@gmail.com>
2016-03-19 12:24:53 +0900

    ピンの設定終わった!

---
+++ Mogura.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+import RPi.GPIO as GPIO
+
+#ボードの番号で設定(目で見て数えるやつ)
+GPIO.setmode(GPIO.BOARD)
+
+#LED定義
+LD1 = 19
+LD2 = 21
+LD3 = 23
+LD4 = 27
+#LEDのポート宣言
+GPIO.setup(LD1, GPIO.OUT)
+GPIO.setup(LD2, GPIO.OUT)
+GPIO.setup(LD3, GPIO.OUT)
+GPIO.setup(LD4, GPIO.OUT)
+
+#SW定義
+SW1 = 31
+SW2 = 33
+SW3 = 35
+SW4 = 37
+#SWのポート宣言
+GPIO.setup(SW1, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+GPIO.setup(SW2, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+GPIO.setup(SW3, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+GPIO.setup(SW4, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+
+#スピーカー定義
+SP = 40
+#スピーカーのポート宣言
+GPIO.setup(SP, GPIO.OUT)
